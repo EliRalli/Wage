565368350ab6bbeb99007be7189886b0baeef164
EliRalli <114882586+EliRalli@users.noreply.github.com>
2022-10-02 22:19:13 -0700

    Add files via upload

---
+++ Wage.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 08:21:35 2020
+
+@author: bofoi
+"""
+Wage= float(input('enter a Wage  '))
+Hours=40
+Weeks=50
+Money=Wage*Hours*Weeks;
+print(Hours*Weeks*Wage);
+print("Wage=$",Wage,"/hr",end='\n')
+print("Annual Salary=$",Money,"/year")
